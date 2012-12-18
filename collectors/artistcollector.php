de8fef323ca9e6dd909b6a66ddc8b9edf8a4951f
thexa4 <thexa4@gmail.com>
2012-12-18 16:39:17 +0000

    Added track numbering to Album collectors

--- collectors/artistcollector.php
+++ collectors/artistcollector.php
@@ -11,11 +11,23 @@ Album::ForAll(function($album){
 	if(!in_array($album->info->type, array('Album', 'Single', 'EP', 'Live', 'Other', 'Remix', 'Compilation')))
 		return;
 
-	$records = scandir($album->path);
-	$count = 0;
-	foreach($records as $record)
-		if($record[0] != '.')
-			$count++;
+	$releases = array();
+	foreach($album->info->releases as $release)
+		$releases[] = new Release($release->id);
+
+	$records = array();
+	foreach($releases as $release)
+		foreach($release->info->medium as $medium)
+			foreach($medium->tracks as $track)
+			{
+				$newrecord = new Record($track->recording->id);
+				if($newrecord->Exists() && !isset($records[$track->recording->id]))
+				{
+					$newrecord->nr = sprintf('%02d%02d', $medium->position, $track->position);
+					$records[$track->recording->id] = $newrecord;
+				}
+			}
+	$count = count($records);
 
 	if($count < Settings::AlbumMinRecords)
 		return;
@@ -35,8 +47,46 @@ Album::ForAll(function($album){
 		print $album->mbid . " added\n";
 	}
 
-	foreach($records as $record)
-		if($record[0] != '.' && !is_file($destfolder . $record) && !is_link($destfolder . $record))
-			symlink($album->path . $record, $destfolder . $record);
+	$maxbest = 0;
+	foreach($releases as $release)
+	{
+		$currentbest = 0;
+		$current = array();
+		foreach($release->info->medium as $medium)
+			foreach($medium->tracks as $track)
+			{
+				if(!isset($records[$track->recording->id]))
+					continue;
+				$currentbest++;
+				$current[$track->recording->id] = sprintf('%02d%02d', $medium->position, $track->position);
+			}
+		if($currentbest > $maxbest)
+		{
+			$maxbest = $currentbest;
+			foreach($current as $id => $nr)
+				$records[$id]->nr = $nr;
+		}
+	}
 
+	// Add new symlinks
+	$touched = array();
+	foreach($records as $id => $record)
+	{
+		$filename = $record->nr . ' - ' . Utils::CleanPath($record->info->title) . '.mp3';
+		$dest = $destfolder . $filename;
+
+		$touched[] = $filename;
+		if(!is_file($dest) && !is_link($dest))
+			symlink($record->file, $dest);
+	}
+
+	// Remove wrong symlinks
+	$dir = scandir($destfolder);
+	foreach($dir as $file)
+	{
+		if($file[0] == '.')
+			continue;
+		if(!in_array($file, $touched))
+			unlink($destfolder . $file);
+	}
 });
